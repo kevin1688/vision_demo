63dc4eb44333851f1771831563a2f83af954afc0
kb18k <jackexi@gmail.com>
2022-12-27 12:07:18 +0800

    Initial Commit

---
+++ vision_demo/vision_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  vision_demoApp.swift
+//  vision_demo
+//
+//  Created by kai wen chen on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct vision_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
